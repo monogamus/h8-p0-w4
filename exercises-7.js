f865f8760a96e148e5b608db28bbeedf1011b2eb
monogamus <monogamus@yahoo.com>
2019-07-17 15:44:45 +0700

    exe7

---
+++ exercises-7.js
@@ -0,0 +1,11 @@
+// Logic Challenge - Mengurutkan Abjad
+function urutkanAbjad(str) {
+  return str.split("").sort().join("");
+}
+
+// TEST CASES
+console.log(urutkanAbjad('hello')); // 'ehllo'
+console.log(urutkanAbjad('truncate')); // 'acenrttu'
+console.log(urutkanAbjad('developer')); // 'deeeloprv'
+console.log(urutkanAbjad('software')); // 'aeforstw'
+console.log(urutkanAbjad('aegis')); // 'aegis'
