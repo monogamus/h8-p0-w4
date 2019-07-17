42841dca7530862cc8267852cc427a5faeb5c023
monogamus <monogamus@yahoo.com>
2019-07-17 15:37:21 +0700

    exe1

---
+++ exercises-1.js
@@ -0,0 +1,17 @@
+// Logic Challenge - Angka Prima
+function angkaPrima(angka) {
+  var kondisi = true;
+  for (var i = 2; i < angka; i++) {
+    if (angka % i === 0) { 
+      kondisi = false; 
+    } 
+  }
+  return kondisi;
+}
+
+// TEST CASES
+console.log(angkaPrima(3)); // true
+console.log(angkaPrima(7)); // true
+console.log(angkaPrima(6)); // false
+console.log(angkaPrima(23)); // true
+console.log(angkaPrima(33)); // false
